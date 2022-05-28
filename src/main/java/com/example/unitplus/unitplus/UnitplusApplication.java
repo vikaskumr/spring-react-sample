c7d55eddf8800e61d1513a0b868e662d8f63bce9
Vikas Kumar <vikas.kumar@energyweb.org>
2022-05-28 10:00:54 +0530

    feat: structure for react and spring boot setup

---
+++ src/main/java/com/example/unitplus/unitplus/UnitplusApplication.java
@@ -0,0 +1,13 @@
+package com.example.unitplus.unitplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unitplus/unitplus/UnitplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unitplus.unitplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
